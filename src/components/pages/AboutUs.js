fc413b2f438797071cbc0394274b43922d22dd06
Nidhi010805 <nidhidevi425@gmail.com>
2025-01-23 19:24:53 +0530

    updated font

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>
+    <div className="aboutus" >
+      <h1 className="heading">About Us</h1>
+      <p className="paracontent">
       The Department of Electrical & Electronics Engineering is dedicated to the current needs of industry with the flexibility to tune its programmes according to different requirements. Application of new technology in various fields is one of the main focuses in the activities of the department. Department of EEE has received grants amounting to Rs. 4.5 Crore in recent past from ERLDC (Grid Controller of India), SERB-DST, ICMR, CCL, UGC, DST, AICTE etc. to strengthen the research facility of the department. The department involves with different industries like TATA Motors, Orient Electric Ltd. Larsen & Toubro Technology Services (LTTS) for knowledge sharing and problem solving.  MoUs have recently been signed with ERLDC, TATA Motors, LTTS, CIL, University of Windsor, Canada.
   
 
-      <h2>Vision</h2>
+      <h2 className="subheading">Vision</h2>
 
 To become an internationally recognized center of excellence in academics, research and technological services in the area of Electrical and Electronics Engineering and related inter-disciplinary fields.
 
-<h2>Mission</h2>
+<h2 className="subheading">Mission</h2>
 
 Imparting strong fundamental concepts to students and motivate them to find innovative solutions to engineering problems independently.
 Developing engineers with managerial attributes capable of applying appropriate technology with responsibility.
@@ -23,11 +23,11 @@ Undergraduate Programme
 
 The Department has a B.Tech. Programme in Electrical & Electronics Engineering.
 
-<h2>Postgraduate Programmes</h2>
+<h2 className="subheading">Postgraduate Programmes</h2>
 
 The Department offers Masters of Technology in Electrical Engineering and Masters of Technology in Electric Vehicle Technology (For TATA Motors Executives). 
 
-<h2>Doctoral Programmes</h2>
+<h2 className="subheading">Doctoral Programmes</h2>
 
 Currently a number of Ph.D. scholars are working in the areas of non-stationary signal analysis, intelligent control techniques applied to automatic generation control and non-linear dynamic systems, development of new architecture for neural networks, phasor measurement units, wide area monitoring system (WAMS), image processing, smart grid, power quality, application of Block Chain Technology, Integration of renewable sources to grid etc.
       </p>
